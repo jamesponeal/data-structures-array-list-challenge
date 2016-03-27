bf8631f1e1b1c032d7b1ee35d37f61d99a710f48
James O'Neal <jamesponeal@gmail.com>
2016-03-27 09:42:06 -0500

    adds completed array-list files

--- array_list.rb
+++ array_list.rb
@@ -4,28 +4,39 @@ class ArrayList
     @array = Array.new(size)
   end
 
-  def size
-    @array.length
-  end
-
   def add(element)
     @array.push(element)
     @array.last
   end
 
   def get(index)
-    raise OutOfBoundsException if index >= self.size || index < 0
+    raise OutOfBoundsException if index >= self.length || index < 0
     @array[index]
   end
 
   def set(index, value)
-    raise OutOfBoundsException if index >= self.size || index < 0
+    raise OutOfBoundsException if index >= self.length || index < 0
     @array[index] = value
     @array[index]
   end
 
   def length
     @array.length
+  end
+
+  def insert(index, value)
+    raise OutOfBoundsException if index > self.length || index < 0
+    shift_right(index)
+    @array[index] = value
+    @array[index]
+  end
+
+  def shift_right(index)
+    position = @array.length
+    while position > index
+      @array[position] = @array[position-1]
+      position -= 1
+    end
   end
 
 end
--- array_list_spec.rb
+++ array_list_spec.rb
@@ -4,19 +4,19 @@ describe ArrayList do
 
   describe "#new" do
     it "generates an array of the given size" do
-      array1 = ArrayList.new(5)
-      expect(array1.size).to eq 5
+      array1 = ArrayList.new(4)
+      expect(array1.length).to eq 4
     end
 
-    it "generates an array of length 8 if no size is given" do
+    it "generates an array of size 8 if no other size is specified" do
       array1 = ArrayList.new
-      expect(array1.size).to eq 8
+      expect(array1.length).to eq 8
     end
   end
 
   describe "#add" do
     it "adds the given value to the end of the array" do
-      array1 = ArrayList.new(5)
+      array1 = ArrayList.new
       expect(array1.add("thing")).to eq "thing"
     end
   end
@@ -60,6 +60,58 @@ describe ArrayList do
       array1 = ArrayList.new(5)
       expect(array1.length).to eq 5
     end
+  end
+
+  describe "#insert" do
+    it "inserts the specified value at the given index" do
+      array1 = ArrayList.new(['A', 'B', 'D', 'E', 'F', 'G'])
+      expect(array1.insert(2, 'C')).to eq 'C'
+      expect(array1.length).to eq 7
+      expect(array1.get(6)).to eq 'G'
+      expect(array1.get(5)).to eq 'F'
+      expect(array1.get(4)).to eq 'E'
+      expect(array1.get(3)).to eq 'D'
+      expect(array1.get(2)).to eq 'C'
+      expect(array1.get(1)).to eq 'B'
+      expect(array1.get(0)).to eq 'A'
+    end
+
+    it "inserts the specified value at the given index" do
+      array1 = ArrayList.new(['B', 'C', 'D', 'E', 'F', 'G'])
+      expect(array1.insert(0, 'A')).to eq 'A'
+      expect(array1.length).to eq 7
+      expect(array1.get(6)).to eq 'G'
+      expect(array1.get(5)).to eq 'F'
+      expect(array1.get(4)).to eq 'E'
+      expect(array1.get(3)).to eq 'D'
+      expect(array1.get(2)).to eq 'C'
+      expect(array1.get(1)).to eq 'B'
+      expect(array1.get(0)).to eq 'A'
+    end
+
+    it "inserts the specified value at the given index" do
+      array1 = ArrayList.new(['A', 'B', 'C', 'D', 'E', 'F'])
+      expect(array1.insert(6, 'G')).to eq 'G'
+      expect(array1.length).to eq 7
+      expect(array1.get(6)).to eq 'G'
+      expect(array1.get(5)).to eq 'F'
+      expect(array1.get(4)).to eq 'E'
+      expect(array1.get(3)).to eq 'D'
+      expect(array1.get(2)).to eq 'C'
+      expect(array1.get(1)).to eq 'B'
+      expect(array1.get(0)).to eq 'A'
+    end
+
+    it "raises an OutOfBoundsException if the index number is too large" do
+      array1 = ArrayList.new(['A', 'B', 'C', 'D'])
+      expect{array1.insert(7, 'X')}.to raise_error(OutOfBoundsException)
+    end
+
+    it "raises an OutOfBoundsException if the index number is too small" do
+      array1 = ArrayList.new(['A', 'B', 'C', 'D'])
+      expect{array1.insert(-1, 'X')}.to raise_error(OutOfBoundsException)
+    end
+
   end
 
 end
